87a685f3596c7493ab51e94881693ccfb701e978
zhuochujie <zhuochujie@zhuochujiedeMacBook-Pro.local>
2026-06-15 16:43:37 +0800

    Update production site configuration

--- src/web3/config.ts
+++ src/web3/config.ts
@@ -1,5 +1,5 @@
-// import { bsc } from 'viem/chains'
-import { bscTestnet as bsc } from 'viem/chains'
+import { bsc } from 'viem/chains'
+// import { bscTestnet as bsc } from 'viem/chains'
 import { createConfig, http, injected } from 'wagmi'
 
 export const wagmiConfig = createConfig({
